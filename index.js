9fd6474c650501e7f4844de27b78b93ec74b5001
Ayush kumar <84299616+ayush462@users.noreply.github.com>
2026-04-09 00:35:32 +0530

    Deploy via DevDeploy

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors({ origin: process.env.FRONTEND_URL || '*', credentials: true }));
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
